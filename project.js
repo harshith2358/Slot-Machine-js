304043cecf0c5c4a2f76f92acca0703eea9004cd
Harshith Reddy <kalluriharshith2006@gmail.com>
2026-02-22 22:36:15 +0800

    Created a deposit function

---
+++ project.js
@@ -0,0 +1,21 @@
+// 1. Deposit some money
+// 2. Determine number of line to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+//7. Play again
+
+////// Functions ////////
+
+// function deposit() {
+//     return 1
+// }
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+  const depositAmount = prompt("Enter a deposit amount: ");
+};
+
+deposit()
